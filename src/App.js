7019842fda586fecd80331c59e7b4dbcb0f6fa47
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2024-01-04 15:07:10 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from "react";
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from "reactflow";
 
-function App() {
+import "reactflow/dist/style.css";
+
+import CustomEdge from "./components/CustomEdge.jsx";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 100, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [
+  { id: "a->b", type: "custom-edge", source: "a", target: "b" },
+];
+
+const edgeTypes = {
+  "custom-edge": CustomEdge,
+};
+
+export default function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (connection) => {
+      const edge = { ...connection, type: "custom-edge" };
+      setEdges((eds) => addEdge(edge, eds));
+    },
+    [setEdges]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        edgeTypes={edgeTypes}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant="none" gap={12} size={1} />
+      </ReactFlow>
     </div>
   );
 }
-
-export default App;
